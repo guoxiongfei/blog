0487d43e20ab29eddab3a5d58de1cc4567a47b1c
GuoXiongfei <409840063@qq.com>
2022-06-05 17:13:31 +0800

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?210cde56e2e4f49278531d8d7fae589a";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
