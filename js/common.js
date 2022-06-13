754a91d7f44405e94822107c360e9e461a241461
GuoXiongfei <409840063@qq.com>
2022-06-14 00:12:16 +0800

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,7 +1,34 @@
+//var blog_host = "http://localhost:8080";
+var blog_host = "http://39.108.226.6:8080";
 var _hmt = _hmt || [];
-(function() {
+(function () {
     var hm = document.createElement("script");
     hm.src = "https://hm.baidu.com/hm.js?210cde56e2e4f49278531d8d7fae589a";
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(hm, s);
 })();
+
+//search
+function _search(w) {
+    document.getElementById("rc_select_0").value = decodeURI(w);
+    jsonp({
+        url: blog_host + "/admin/s?keywords=" + w + "&domain=www.guoxiongfei.cn",
+        data: {},
+        success: function (obj) {
+            console.log("obj=", obj);
+        }
+    });
+}
+
+function fn_s(obj) {
+    var w = document.getElementById("rc_select_0").value.replaceAll(" ", "");
+    if (w.length < 1) {
+        document.getElementById("rc_select_0").focus();
+        return;
+    }
+    w = w.replace("<", "").replace(">", "");
+    if (window.location.href.indexOf("/search.html") < 0) {
+        window.location.href = "/search.html?q=" + w;
+    }
+    _search(w);
+}
